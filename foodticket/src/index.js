5c0c4fb535d2038bc8a0446896299b15a90fdd5a
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-10 16:23:46 +0900

    kakao map 예시 추가

--- foodticket/src/index.js
+++ foodticket/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import MainMenu from "./pages/MainMenu/MainMenu.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <MainMenu />
+  </>,
   document.getElementById("root")
 );
---
+++ foodticket/src/pages/MainMenu/MainMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MainMenu.css";
+import Map from "../../Components/map/Map.js";
+
+function MainMenu() {
+  return (
+    <div className="mainmenu">
+      <Map />
+    </div>
+  );
+}
+
+export default MainMenu;
