c3f3f0c8e44787767405da79d9c7d770aa3a0b69
Kyuhyeon <narvik37@naver.com>
2020-08-12 15:18:47 +0900

    MainMenu틀 완성

--- foodticket/src/index.js
+++ foodticket/src/index.js
@@ -4,20 +4,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import MainMenu from "./pages/MainMenu/MainMenu.js";
 import SignUp from "./pages/SignUp/SignUp";
+import Customer from "./pages/Customer"
 
-import testMain from "./pages/MainMenu/testMain.js"
 
 ReactDOM.render(
   <>
     <Router>
       <Switch>
-<<<<<<< HEAD
-        <Route path="/" exact component={testMain} />
-=======
         <Route path="/" exact component={MainMenu} />
         <Route path="/signUp" exact component={SignUp} />
         <Route path="/" exact component={Customer} />
->>>>>>> 50623866a83e54c1cc88bd2e2b0a82734d4f9a29
       </Switch>
     </Router>
   </>,
