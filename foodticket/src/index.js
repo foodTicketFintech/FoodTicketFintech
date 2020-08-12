fc7450d2e267825c62234cd19cb8fd0b95972319
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-12 10:41:27 +0900

    router추가

--- foodticket/src/index.js
+++ foodticket/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import ReactDOM from "react-dom";
 import "./index.css";
 import MainMenu from "./pages/MainMenu/MainMenu.js";
 
 ReactDOM.render(
   <>
-    <MainMenu />
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MainMenu} />
+      </Switch>
+    </Router>
   </>,
   document.getElementById("root")
 );
