69d35ea7e459dbf421f4cf90d22120fb6d202514
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-13 23:51:47 +0900

    DB localhost:3000 cors 허용

--- foodticket/src/server/server.js
+++ foodticket/src/server/server.js
@@ -1,25 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
-const db = require('./config/DB');
+const db = require("./config/DB");
+const cors = require("cors");
 
-app.get('/api/host', (req, res) => {
-    res.send({ host : 'hwi'});
-})
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, // true로 하면 설정한 내용을 response 헤더 추가
+};
+app.use(cors());
 
-app.get('/api/test', (req, res) => {
-    db.query("select * from customer", (err, data) => {
-        if(!err) {
-            res.header("Access-Control-Allow-Origin","*");
-            res.send(data);
-        } else {
-            console.log(err);
-            res.send(err);
-        }
+app.get("/api/host", (req, res) => {
+  res.send({ host: "hwi" });
+});
 
-    })
-})
+app.get("/api/test", (req, res) => {
+  db.query("select * from customer", (err, data) => {
+    if (!err) {
+      res.send(data);
+    } else {
+      console.log(err);
+      res.send(err);
+    }
+  });
+});
 
-app.listen(PORT, ()=> {
-    console.log("Server On : http://localhost:",PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server On : http://localhost:", PORT);
+});
