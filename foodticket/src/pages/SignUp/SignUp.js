40581556e9dc68c8155cdaff2518431d5c828293
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-13 20:30:39 +0900

    회원가입 로직 function -> class 변경

--- foodticket/src/pages/SignUp/SignUp.js
+++ foodticket/src/pages/SignUp/SignUp.js
@@ -1,7 +1,6 @@
 /*global kakao */
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./SignUp.css";
-// import kakao from "//dapi.kakao.com/v2/maps/sdk.js?appkey=ac73d6837cb1a759883d1884b65be73c";
 /*
 Customer table구조 
 
@@ -12,152 +11,158 @@ name (char) : 사용자 이름
 birth (char) : 생년월일
 address (char) : 주소
 
-TODO : 주소 받으면 x좌표와 y좌표로 변환해주는 로직 필요함
-position_x (char) : 주소 x좌표
-position_y (char) : 주소 y좌표
-
 */
 
-const SignUp = () => {
-  const [id, setId] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
-  const [birth, setBirth] = useState("");
-  const [address, setAddress] = useState("");
-  //   const [term, setTerm] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
-  const [termError, setTermError] = useState(false);
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      name: "",
+      password: "",
+      passwordCheck: "",
+      birth: "",
+      address: "",
+      passwordError: false,
+      termError: false,
+    };
+  }
 
-  const onSubmit = async (e) => {
+  onSubmit = async (e) => {
     e.preventDefault();
     /**검증 로직 만들기
      * 1. 비밀번호와 비밀번호 체크가 다를 경우를 검증한다
      * 2. 약관 동의를 확인한다.
      */
-    if (password !== passwordCheck) {
-      return setPasswordError(true);
+    if (this.state.password !== this.state.passwordCheck) {
+      return this.setState({ passwordError: true });
     }
     // if (!term) {
     //   return setTermError(true);
     // }
-    console.log({
-      id,
-      name,
-      password,
-      passwordCheck,
-    });
+    console.log(this.state.id, this.state.name, this.state.password, this.state.passwordCheck);
     // TODO : 1. Kakao Map API 사용 -> 좌표 받아와야 함
 
     // TODO : 2. DB에 저장하는 부분 작성
   };
 
   // Coustom Hook 이전
-  const onChangeId = (e) => {
-    setId(e.target.value);
+  onChangeId = (e) => {
+    this.setState({ id: e.target.value });
   };
-  const onChangeName = (e) => {
-    setName(e.target.value);
+  onChangeName = (e) => {
+    this.setState({ name: e.target.value });
     // SVGComponentTransferFunctionElement(e.target.value);
   };
-  const onChangePassword = (e) => {
-    setPassword(e.target.value);
+  onChangePassword = (e) => {
+    this.setState({ password: e.target.value });
   };
-  const onChangePasswordChk = (e) => {
+  onChangePasswordChk = (e) => {
     //비밀번호를 입력할때마다 password 를 검증하는 함수
-    setPasswordError(e.target.value !== password);
-    setPasswordCheck(e.target.value);
+    if (e.target.value !== this.state.password) {
+      this.setState({ passwordError: true });
+    } else {
+      this.setState({ passwordError: false });
+    }
+    this.setState({ passwordCheck: e.target.value });
   };
-  const onChangeBrith = (e) => {
-    setBirth(e.target.value);
+  onChangeBrith = (e) => {
+    this.setState({ birth: e.target.value });
   };
 
-  const onChangeAddress = (e) => {
-    setTermError(false);
-    setAddress(e.target.value);
+  onChangeAddress = (e) => {
+    // setTermError(false);
+    this.setState({ address: e.target.value });
   };
 
-  //   const onChangeTerm = (e) => {
+  //   onChangeTerm = (e) => {
   //     //체크박스 초기화
   //     setTermError(false);
   //     setTerm(e.target.checked);
   //   };
+  componentDidMount() {}
 
-  return (
-    <>
-      <form onSubmit={onSubmit} style={{ padding: 10 }}>
-        {/* ID input tag */}
-        <div>
-          <label htmlFor="user-id">아이디</label>
-          <br />
-          <input name="user-id" value={id} required onChange={onChangeId} />
-        </div>
+  render() {
+    return (
+      <>
+        <form onSubmit={this.onSubmit} style={{ padding: 10 }}>
+          {/* ID input tag */}
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <input name="user-id" required onChange={(e) => this.onChangeId(e)} />
+          </div>
 
-        {/* Name input tag */}
-        <div>
-          <label htmlFor="user-name">이름</label>
-          <br />
-          <input name="user-name" value={name} required onChange={onChangeName} />
-        </div>
+          {/* Name input tag */}
+          <div>
+            <label htmlFor="user-name">이름</label>
+            <br />
+            <input name="user-name" required onChange={(e) => this.onChangeName(e)} />
+          </div>
 
-        {/* PASSWORD input tag */}
-        <div>
-          <label htmlFor="user-password">비밀번호</label>
-          <br />
-          <input
-            name="user-password"
-            type="password"
-            value={password}
-            required
-            onChange={onChangePassword}
-          />
-        </div>
+          {/* PASSWORD input tag */}
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <input
+              name="user-password"
+              type="password"
+              required
+              onChange={(e) => this.onChangePassword(e)}
+            />
+          </div>
 
-        {/* PASSWORD_CHECK input tag */}
-        <div>
-          <label htmlFor="user-password-check">비밀번호체크</label>
-          <br />
-          <input
-            name="user-password-check"
-            type="password"
-            value={passwordCheck}
-            required
-            onChange={onChangePasswordChk}
-          />
-          {passwordError && <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>}
-        </div>
+          {/* PASSWORD_CHECK input tag */}
+          <div>
+            <label htmlFor="user-password-check">비밀번호체크</label>
+            <br />
+            <input
+              name="user-password-check"
+              type="password"
+              required
+              onChange={(e) => this.onChangePasswordChk(e)}
+            />
+            {this.state.passwordError && (
+              <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>
+            )}
+          </div>
 
-        {/* birth tag */}
-        <div>
-          <label htmlFor="user-birth">생년월일</label>
-          <br />
-          <input name="user-birth" type="birth" value={birth} required onChange={onChangeBrith} />
-        </div>
+          {/* birth tag */}
+          <div>
+            <label htmlFor="user-birth">생년월일</label>
+            <br />
+            <input
+              name="user-birth"
+              type="birth"
+              required
+              onChange={(e) => this.onChangeBrith(e)}
+            />
+          </div>
 
-        {/* address */}
-        <div>
-          <label htmlFor="user-address">주소</label>
-          <br />
-          <input
-            name="user-address"
-            type="address"
-            value={address}
-            required
-            onChange={onChangeAddress}
-          />
-        </div>
-        {/* <div>
+          {/* address */}
+          <div>
+            <label htmlFor="user-address">주소</label>
+            <br />
+            <input
+              name="user-address"
+              type="address"
+              required
+              onChange={(e) => this.onChangeAddress(e)}
+            />
+          </div>
+          {/* <div>
           <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
             동의 합니까?
           </Checkbox>
           {termError && <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>}
         </div> */}
-        <div style={{ marginTop: 10 }}>
-          <input type="submit" value="로그인" action={onSubmit} />
-        </div>
-      </form>
-    </>
-  );
-};
+          <div style={{ marginTop: 10 }}>
+            <input type="submit" value="로그인" action={(e) => this.onSubmit(e)} />
+          </div>
+        </form>
+      </>
+    );
+  }
+}
 
 export default SignUp;
---
+++ foodticket/src/pages/SignUp/SignUp2.js
@@ -0,0 +1,158 @@
+/*global kakao */
+import React, { useState } from "react";
+import "./SignUp.css";
+/*
+Customer table구조 
+
+id (int) : 기본키
+email (char) : ID 역할
+password (char) : PASSWORD
+name (char) : 사용자 이름
+birth (char) : 생년월일
+address (char) : 주소
+
+*/
+
+const SignUp = () => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [birth, setBirth] = useState("");
+  const [address, setAddress] = useState("");
+  //   const [term, setTerm] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [termError, setTermError] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    /**검증 로직 만들기
+     * 1. 비밀번호와 비밀번호 체크가 다를 경우를 검증한다
+     * 2. 약관 동의를 확인한다.
+     */
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    // if (!term) {
+    //   return setTermError(true);
+    // }
+    console.log({
+      id,
+      name,
+      password,
+      passwordCheck,
+    });
+    // TODO : 1. Kakao Map API 사용 -> 좌표 받아와야 함
+
+    // TODO : 2. DB에 저장하는 부분 작성
+  };
+
+  // Coustom Hook 이전
+  const onChangeId = (e) => {
+    setId(e.target.value);
+  };
+  const onChangeName = (e) => {
+    setName(e.target.value);
+    // SVGComponentTransferFunctionElement(e.target.value);
+  };
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const onChangePasswordChk = (e) => {
+    //비밀번호를 입력할때마다 password 를 검증하는 함수
+    setPasswordError(e.target.value !== password);
+    setPasswordCheck(e.target.value);
+  };
+  const onChangeBrith = (e) => {
+    setBirth(e.target.value);
+  };
+
+  const onChangeAddress = (e) => {
+    setTermError(false);
+    setAddress(e.target.value);
+  };
+
+  //   const onChangeTerm = (e) => {
+  //     //체크박스 초기화
+  //     setTermError(false);
+  //     setTerm(e.target.checked);
+  //   };
+
+  return (
+    <>
+      <form onSubmit={onSubmit} style={{ padding: 10 }}>
+        {/* ID input tag */}
+        <div>
+          <label htmlFor="user-id">아이디</label>
+          <br />
+          <input name="user-id" value={id} required onChange={onChangeId} />
+        </div>
+
+        {/* Name input tag */}
+        <div>
+          <label htmlFor="user-name">이름</label>
+          <br />
+          <input name="user-name" value={name} required onChange={onChangeName} />
+        </div>
+
+        {/* PASSWORD input tag */}
+        <div>
+          <label htmlFor="user-password">비밀번호</label>
+          <br />
+          <input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+
+        {/* PASSWORD_CHECK input tag */}
+        <div>
+          <label htmlFor="user-password-check">비밀번호체크</label>
+          <br />
+          <input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordChk}
+          />
+          {passwordError && <div style={{ color: "red" }}>비밀번호가 일치하지 않습니다.</div>}
+        </div>
+
+        {/* birth tag */}
+        <div>
+          <label htmlFor="user-birth">생년월일</label>
+          <br />
+          <input name="user-birth" type="birth" value={birth} required onChange={onChangeBrith} />
+        </div>
+
+        {/* address */}
+        <div>
+          <label htmlFor="user-address">주소</label>
+          <br />
+          <input
+            name="user-address"
+            type="address"
+            value={address}
+            required
+            onChange={onChangeAddress}
+          />
+        </div>
+        {/* <div>
+          <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
+            동의 합니까?
+          </Checkbox>
+          {termError && <div style={{ color: "red" }}>약관에 동의하셔야 합니다.</div>}
+        </div> */}
+        <div style={{ marginTop: 10 }}>
+          <input type="submit" value="로그인" action={onSubmit} />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SignUp;
