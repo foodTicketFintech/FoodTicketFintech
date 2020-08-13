31198b1af6f3020e286345bd67a55fc428d5ee63
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-13 19:23:23 +0900

    템플릿 추가

---
+++ foodticket/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import ScrollToTopRoute from "./ScrollToTopRoute";
+import Home from "./pages/Home/Home";
+import SignUp from "./pages/SignUp/SignUp";
+/*------ Pages-----*/
+
+class App extends Component {
+  componentDidMount() {
+    this.props.hideLoader();
+  }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <ScrollToTopRoute exact={true} path={"/"} component={Home} />
+          <ScrollToTopRoute exact={true} path={"/SignUp"} component={SignUp} />
+          {/* <ScrollToTopRoute path="/Digital-marketing" component={DigitalMarketing} />
+          <ScrollToTopRoute path="/Payment-processing" component={PaymentProcessing} /> */}
+          {/* <ScrollToTopRoute component={NotFound} /> */}
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ foodticket/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Map from "../../Components/map/Map";
+//import CustomerInfo from "./CustomerInfo.js";
+
+function MainMenu() {
+  return (
+    <div className="mainmenu">
+      <Map />
+    </div>
+  );
+}
+
+export default MainMenu;
