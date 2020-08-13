05a473e101b2fd25999ccf7ca082cea73e255d5a
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-14 01:33:08 +0900

    DB에 body-parser추가, req.body값 확인

--- foodticket/src/server/server.js
+++ foodticket/src/server/server.js
@@ -3,18 +3,35 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 const db = require("./config/DB");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
+// cors 허용
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true, // true로 하면 설정한 내용을 response 헤더 추가
 };
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/api/host", (req, res) => {
   res.send({ host: "hwi" });
 });
 
 app.get("/api/test", (req, res) => {
+  db.query("select * from customer", (err, data) => {
+    if (!err) {
+      res.send(data);
+    } else {
+      console.log(err);
+      res.send(err);
+    }
+  });
+});
+
+app.post("/customer/join", (req, res) => {
+  console.log(req.body);
   db.query("select * from customer", (err, data) => {
     if (!err) {
       res.send(data);
