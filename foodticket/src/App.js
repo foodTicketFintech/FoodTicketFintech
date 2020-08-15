fe6c88baf78dc6b32cf65141c3bf60afbd6f3d47
rheew <47098896+rheew@users.noreply.github.com>
2020-08-15 16:12:45 +0900

    App revise home root

--- foodticket/src/App.js
+++ foodticket/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import ScrollToTopRoute from "./ScrollToTopRoute";
-import Home from "./pages/Home/Home";
+import homeHosting from "./pages/Home/HomeHosting";
 import SignUp from "./pages/SignUp/SignUp";
 import Login from "./pages/Login/Login";
 
@@ -13,40 +13,21 @@ import StoreReg from "./pages/StoreReg/StoreReg.js";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      logged: false,
-      onLogin: this.onLogin,
-      onLogout: this.onLogout,
-    };
   }
   componentDidMount() {
     this.props.hideLoader();
   }
-
-  onLogin = () => {
-    this.setState({
-      logged: true,
-    });
-  };
-
-  inLogout = () => {
-    this.setState({
-      logged: false,
-    });
-
-    // TODO : sessionstorage내용 없애야함
-  };
-
   render() {
     return (
       <Router>
         <Switch>
-          <ScrollToTopRoute exact={true} path={"/"} component={Home} />
+          <ScrollToTopRoute exact={true} path={"/"} component={homeHosting} />
           <ScrollToTopRoute exact={true} path={"/SignUp"} component={SignUp} />
-          <ScrollToTopRoute exact={true} path={"/Login"} component={Login} state={this.state} />
+          <ScrollToTopRoute exact={true} path={"/Login"} component={Login} />
           <ScrollToTopRoute path="/Main" exact component={MainMenu} />
           <ScrollToTopRoute path="/Customer" exact component={Customer} />
           <ScrollToTopRoute pathh="/StoreReg" exact component={StoreReg} />
+          {/* <ScrollToTopRoute component={NotFound} /> */}
 
           {/* <ScrollToTopRoute path="/Digital-marketing" component={DigitalMarketing} />
           <ScrollToTopRoute path="/Payment-processing" component={PaymentProcessing} /> */}
