a118ad568379b5aa83eedba33938ab4c2f6d8377
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-25 00:51:05 +0900

    qrcode 생성 추가

--- foodticket/src/Components/Order/OrderContent.js
+++ foodticket/src/Components/Order/OrderContent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Reveal from "react-reveal/Reveal/";
+import QRCode from "qrcode.react";
 
 class OrderContent extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class OrderContent extends Component {
       selectFoodName: [],
       foodName: [],
       price: 0,
+      qrcode: false,
     };
   }
 
@@ -64,6 +66,11 @@ class OrderContent extends Component {
         this.setState({ price: this.state.foodName[i].price });
       }
     }
+  };
+
+  makeQrcode = (e) => {
+    e.preventDefault();
+    this.setState({ qrcode: true });
   };
 
   componentDidMount() {
@@ -146,41 +153,22 @@ class OrderContent extends Component {
                 <div className="info_item">
                   <h6>가격</h6>
                   <p>{this.state.price}</p>
+                  <button
+                    onClick={(e) => this.makeQrcode(e)}
+                    className="seo_btn seo_btn_two btn_hover wow fadeInRight"
+                  >
+                    QRcode 만들기
+                  </button>
                 </div>
               </div>
             </div>
-            <div className="col-lg-7">
-              <div className="details_content">
-                <div className="sec_title">
-                  <p className="f_400 f_size_15">
-                    He lost his bottle a load of old tosh cup of tea brolly bog-standard matie boy
-                    blow off the little rotter morish, haggle hotpot skive off cuppa don't get
-                    shirty with me off his nut the full monty. Starkers morish down the pub such a
-                    fibber quaint gosh Harry boot owt to do with me the little rotter, baking cakes
-                    Eaton ummm I'm telling pardon me the bee's knees vagabond Oxford chap, A bit of
-                    how's your father bog-standard hanky panky daft well lavatory bubble and squeak
-                    the full monty. That say nice one grub cup of tea so I said barmy only a quid, I
-                    it's your round gutted mate cup of char golly gosh dropped a clanger my good
-                    sir, James Bond happy days brilliant blimey I is. Boot Jeffrey cockup the BBC
-                    pardon me victoria sponge Why chip shop what a load of rubbish pukka brolly
-                    cuppa tickety-boo bog-standard cheesed off posh, bugger Eaton William smashing
-                    knackered bog bonnet bobby bender cobblers only a quid baking cakes the full
-                    monty pardon you.{" "}
-                  </p>
-                  <p className="f_400 f_size_15">
-                    Twit bonnet Jeffrey hunky-dory gormless chancer bog-standard spiffing good time,
-                    young delinquent Charles don't get shirty with me the BBC is brown bread off his
-                    nut a load of old tosh, chap grub bog skive off pardon me bleeder. Lavatory on
-                    your bike mate happy days the little rotter arse over tit no biggie at public
-                    school wind up car boot bamboozled well barmy bleeder the wireless bugger,
-                    cockup blatant David it's all gone to pot morish mush sloshed boot A bit of
-                    how's your father skive off cheers a load of old tosh. No biggie mush I don't
-                    want no agro it's your round cack boot say, the full monty mufty such a fibber
-                    up the duff Why, Eaton pardon me spiffing blower brown bread.
-                  </p>
-                </div>
+            {this.state.qrcode ? (
+              <div className="col-lg-7">
+                <QRCode size={400} value={this.state.foodSelect} />
               </div>
-            </div>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       </section>
